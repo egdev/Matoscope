025e959294e4f5595eecfc903d0bc42055f3248e
Eric <eric@legrenier.org>
2011-06-07 13:26:05 +0100

    Initialisation Matoscope

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matoscope::Application.config.secret_token = '46e0c78a33e3a85cfaea477252e1a0b9b56d9f4a4550d6d767812188853edcccdb7f33bd191cc5d547ed605252e1d7ffb0b13a4c73635a0a2012b9818a63a5c3'
